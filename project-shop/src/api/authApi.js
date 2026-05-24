c2fbfeb673de0a7a5e49780a577e11c3ae5583df
oalisa <Oalisa72@yandex.ru>
2026-05-24 15:48:17 +0500

    configure frontend auth api proxy

--- project-shop/src/api/authApi.js
+++ project-shop/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authHttp = axios.create({
-  baseURL: import.meta.env.VITE_AUTH_API_URL || "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_AUTH_API_URL || "/api",
   timeout: 5000,
 });
 
