e2897f62985946dda8af93215a70be25b07110d9
oalisa <Oalisa72@yandex.ru>
2026-05-24 18:02:37 +0500

    return current user from protected user endpoint

--- project-shop/backend/controllers/user.controller.js
+++ project-shop/backend/controllers/user.controller.js
@@ -1,3 +1,3 @@
 export const getUser = (req, res) => {
-  res.send("user endpoint");
+  res.json({ user: req.user });
 };
--- project-shop/backend/routes/user.route.js
+++ project-shop/backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { getUser } from "../controllers/user.controller.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", getUser);
+router.get("/", protect, getUser);
 
 export default router;
